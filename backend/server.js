03e20276663bb22a6b0eb1bfea7ecd719527b228
minhchuong32 <chuongminh3225@gmail.com>
2025-06-24 23:01:51 +0700

    app.put [update product]

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,24 @@ app.delete("/api/products/:id", async (req, res) => {
     }
 })
 
+app.put("/api/products/:id", async(req, res) => {
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({success: false, message: "Invalid Product Id"})
+    }
+
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(id, product, {new:true})
+        return res.status(200).json({success: true, message: "Update Successfully", data: updateProduct})
+    }
+    catch(e) {
+        console.log(`Server Error: ${e.message}`)
+        return res.status(500).json({success: false, message:"Server Error"})
+    }
+})
+
 
 app.listen(5000, () => {
     connectDB();
